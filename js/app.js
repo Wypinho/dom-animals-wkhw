08612ee27270bcd95b649ccb4b8350e4cbbb20d2
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-01-10 15:10:51 +0000

    delete working, trying to do radio buttons

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
   const newItemform = document.querySelector('#new-item-form');
   newItemform.addEventListener('submit', formSubmission);
+
+  const button = document.querySelector('button#delete-all');
+  button.addEventListener('click', deleteActivation);
 });
 
 const formSubmission = function (event) {
   event.preventDefault();
-  const animal = document.createElement('li')
-    animal.textContent = `Name: ${event.target.name.value},
-                        Species: ${event.target.species.value},
-                        Continent; ${event.target.continent.value}`
 
-    const list = document.querySelector('ul#endangered-animals');
-    list.appendChild(animal)
-    console.log(event);
+  const animal = createAnimal(event.target);
+  const list = document.querySelector('ul#endangered-animals');
+  list.appendChild(animal);
+
+  event.target.reset();
+};
+
+const createAnimal = function (form) {
+  const animal = document.createElement('li');
+  animal.classList.add('animal');
+
+  const name = document.createElement('h2');
+  name.textContent = form.name.value;
+  animal.appendChild(name);
+
+  const species = document.createElement('h3');
+  species.textContent = form.species.value;
+  animal.appendChild(species);
+
+  const continent = document.createElement('p');
+  continent.textContent = form.continent.value;
+  animal.appendChild(continent);
+
+  return animal;
+};
+
+const deleteActivation = function () {
+  const list = document.querySelector('ul#endangered-animals');
+  list.innerHTML = '';
 };
